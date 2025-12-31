24df3b9976e7bafa89c2d23d8c16606e372aa68f
savavip85 <59269251+savavip85@users.noreply.github.com>
2025-12-31 18:28:04 +0700

    Update swift-nio-ssh dependency to apple's repository
    Replaced swift-nio-ssh dependency with updated version from apple's repository.

--- Package.swift
+++ Package.swift
@@ -17,7 +17,11 @@ let package = Package(
     ],
     dependencies: [
         // .package(path: "/Users/joannisorlandos/git/joannis/swift-nio-ssh"),
-        .package(url: "https://github.com/Joannis/swift-nio-ssh.git", "0.3.4" ..< "0.4.0"),
+        .package(
+    url: "https://github.com/apple/swift-nio-ssh.git",
+    from: "0.12.0"
+),
+
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.2.0"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "3.12.3"),
